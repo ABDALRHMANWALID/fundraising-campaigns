5cf0e0c88716dcddf55892aafac5d1afe5d6b920
Abdalrhman Walid <bodygrapic@gmail.com>
2025-08-20 22:02:31 +0300

    Create campaign-card component

---
+++ src/app/shared/campaigns/campaign-card/campaign-card.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-campaign-card',
+  imports: [MatProgressBarModule, RouterLink],
+  templateUrl: './campaign-card.html',
+  styleUrl: './campaign-card.scss'
+})
+export class CampaignCard {
+  imageUrl = input.required<string>();
+  id = input.required<number>();
+  name = input.required<string>();
+  goal = input.required<number>();
+  currentAmount = input.required<number>();
+}
