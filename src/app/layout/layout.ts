461a9965a96a5018cb5f4a19c5a6e4ab6146b711
Abdalrhman Walid <bodygrapic@gmail.com>
2025-08-20 21:58:40 +0300

    Add Side Nav

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -3,13 +3,17 @@ import { Footer } from '../shared/footer/footer';
 import { Header } from '../shared/header/header';
 import { RouterOutlet } from '@angular/router';
 import { NewsSubscription } from '../shared/news-subscription/news-subscription';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet, Footer, NewsSubscription],
+  imports: [
+    Header,
+    RouterOutlet,
+    Footer,
+    NewsSubscription,
+    MatSidenavModule,
+  ],
   templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  styleUrl: './layout.scss',
 })
-export class Layout {
-
-}
+export class Layout {}
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
-
+  drawer = input.required<MatDrawer>();
 }
