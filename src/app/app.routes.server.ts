296a9119ed098e3fe372336e4f6ad5354be25a95
Abdalrhman Walid <bodygrapic@gmail.com>
2025-08-21 00:15:06 +0300

    Change render technique to campaign-details

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'campaigns/:id',
+    renderMode: RenderMode.Client
   }
 ];
