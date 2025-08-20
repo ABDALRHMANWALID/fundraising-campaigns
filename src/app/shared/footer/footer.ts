8b41916c2184d207cdef4946675109d7fa7b4a51
Abdalrhman Walid <bodygrapic@gmail.com>
2025-08-20 21:23:42 +0300

    Add Footer

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,11 +1,132 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatDividerModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
+  currentYear = new Date().getFullYear();
 
+  sections = [
+    {
+      title: 'Product',
+      links: [
+        {
+          title: 'Overview',
+          link: '/overview',
+        },
+        {
+          title: 'Features',
+          link: '/features',
+        },
+        {
+          title: 'Solutions',
+          link: '/solutions',
+        },
+
+        {
+          title: 'Tutorials',
+          link: '/tutorials',
+        },
+        {
+          title: 'Pricing',
+          link: '/pricing',
+        },
+        {
+          title: 'Releases',
+          link: '/releases',
+        },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        {
+          title: 'About',
+          link: '/about'
+        },
+        {
+          title: 'Careers',
+          link: '/careers'
+        },
+        {
+          title: 'Press',
+          link: '/press'
+        },
+        {
+          title: 'News',
+          link: '/news'
+        },
+        {
+          title: 'Media Kit',
+          link: '/media-kit'
+        },
+        {
+          title: 'Contact',
+          link: '/contact'
+        },
+      ]
+    },
+    {
+      title: 'Resources',
+      links: [
+        {
+          title: 'Blog',
+          link: '/blog'
+        },
+        {
+          title: 'Newsletter',
+          link: '/newsletter'
+        },
+        {
+          title: 'Events',
+          link: '/events'
+        },
+        {
+          title: 'Help Center',
+          link: '/help-center'
+        },
+        {
+          title: 'Tutorials',
+          link: '/tutorials'
+        },
+        {
+          title: 'Support',
+          link: '/support'
+        },
+      ]
+    },
+    {
+      title: 'Legal',
+      links: [
+        {
+          title: 'Terms',
+          link: '/terms'
+        },
+        {
+          title: 'Privacy',
+          link: '/privacy'
+        },
+        {
+          title: 'Cookies',
+          link: '/cookies'
+        },
+        {
+          title: 'Licenses',
+          link: '/licenses'
+        },
+        {
+          title: 'Settings',
+          link: '/settings'
+        },
+        {
+          title: 'Contact',
+          link: '/contact'
+        },
+      ]
+    },
+  ];
 }
