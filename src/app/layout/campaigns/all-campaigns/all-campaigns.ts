e8d552a818fb918d11e66133a96a77634d40b6db
Abdalrhman Walid <bodygrapic@gmail.com>
2025-08-20 22:15:17 +0300

    Add All Campaigns Component

--- src/app/layout/campaigns/all-campaigns/all-campaigns.ts
+++ src/app/layout/campaigns/all-campaigns/all-campaigns.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, resource } from '@angular/core';
+import { CampaignCard } from '../../../shared/campaigns/campaign-card/campaign-card';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-all-campaigns',
-  imports: [],
+  imports: [CampaignCard],
   templateUrl: './all-campaigns.html',
-  styleUrl: './all-campaigns.scss'
+  styleUrl: './all-campaigns.scss',
 })
 export class AllCampaigns {
-
+  campaigns = resource({
+    loader: async (): Promise<any> =>
+      fetch(`${environment.uri}api/campaigns`).then((res) => res.json()),
+  });
 }
